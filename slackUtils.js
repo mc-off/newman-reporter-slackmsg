5be945855afe5e7e64ee9b07270b5b544b07fc52
Artem Makov <36981923+mc-off@users.noreply.github.com>
2024-02-14 16:42:38 +0300

    Change title to test message

--- slackUtils.js
+++ slackUtils.js
@@ -22,7 +22,7 @@ function slackMessage(stats, timings, failures, maxMessageSize, collection, envi
         {
             "mrkdwn_in": ["text"],
             "color": "#008000",
-            "author_name": "Conversation testing",
+            "author_name": "Тупа тест",
             "title": ":white_check_mark: Conversation tests passed :white_check_mark:"
         }
     ]`
